b04956fd17b959b7995e8f14daf466351eed3407
Dante <daniellewinski95@gmail.com>
2017-03-11 13:40:40 +0100

    Asnwer spawning.

--- PolygonJam/Assets/Scripts/Game.cs
+++ PolygonJam/Assets/Scripts/Game.cs
@@ -10,10 +10,18 @@ public class Game : MonoBehaviour
 	[SerializeField]
 	GameObject border = null;
 
+	[SerializeField]
+	GameObject answer = null;
+
+
 	List<GameObject> borders = new List<GameObject>();
+	List<GameObject> answers = new List<GameObject>();
+	GameObject player = null;
 
 	void Start ()
 	{
+		player = GameObject.FindGameObjectWithTag("Player");
+
 		borders.Add(Instantiate(border, new Vector3(0, Mathf.Ceil(gridSize.y/2+1), 0), transform.rotation) as GameObject);
 		borders.Add(Instantiate(border, new Vector3(0, -Mathf.Ceil(gridSize.y/2+1), 0), transform.rotation) as GameObject);
 		borders.Add(Instantiate(border, new Vector3(Mathf.Ceil(gridSize.x / 2+1), 0, 0), transform.rotation) as GameObject);
@@ -34,6 +42,26 @@ public class Game : MonoBehaviour
 	
 	void Update ()
 	{
-		
+		if (Input.GetKey(KeyCode.Space))
+			NextQuestion();
+	}
+
+	void NextQuestion()
+	{
+		foreach(GameObject obj in answers)
+			Destroy(obj);
+
+		answers.Add(Instantiate(answer, RandomPosition(), transform.rotation) as GameObject);
+	}
+
+	Vector3 RandomPosition()
+	{
+		Vector3 position;
+		do
+		{
+			position = new Vector3(Random.RandomRange(-gridSize.x, gridSize.x) / 2, Random.RandomRange(-gridSize.y, gridSize.y) / 2, -1);
+		}
+		while (Mathf.Abs(position.x - player.transform.position.x) < 1 || Mathf.Abs(position.y - player.transform.position.y) < 1);
+		return position;
 	}
 }
