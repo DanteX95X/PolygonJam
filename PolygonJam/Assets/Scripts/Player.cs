512b8e93263d10ba0951739e18944cf373a9ddc9
Dante <daniellewinski95@gmail.com>
2017-03-11 12:51:10 +0100

    Player and grid prefab.

---
+++ PolygonJam/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	Rigidbody2D body = null;
+
+	[SerializeField]
+	float speed = 5;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log(Input.GetAxis("Horizontal") + " " + Input.GetAxis("Vertical"));
+		body.velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+
+	}
+}
